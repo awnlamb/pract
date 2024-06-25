932c5a055e092b32df2de85b8dbe62252b0c8cd1
awnlamb <nick.kon.120@gmail.com>
2024-06-26 01:41:24 +0300

    Add files via upload

--- noddd4/mynod4/nodnok.cpp
+++ noddd4/mynod4/nodnok.cpp
@@ -1,5 +1,5 @@
 #include "nodnok.h"
-#include <cmath>
+//#include <cmath>
 nodnok::nodnok() {}
 nodnok::nodnok(int c,int d)
 {
@@ -7,23 +7,34 @@ nodnok::nodnok(int c,int d)
 }
 int nodnok::nod()
 {
-    unsigned int c=abs(a);
-    unsigned int d=abs(b);
-    if (c % d == 0) return d;
-    if (d % c == 0) return c;
-    if (c > d)
+    if (a<0) a*=-1;
+    if (b<0) b*=-1;
+    if (b == 0)
+        return a;
+    else
     {
-        c%=d;
+        int t=a;
+        a=b;
+        b=t%b;
         return this->nod();
+    }
+    /*if (a % b == 0) return b;
+    if (b % a == 0) return a;
+    if (a > b)
+    {
+        a%=b;
+        return abs(this->nod());
     }
     else
     {
-        d%=c;
+        b%=a;
         return abs(this->nod());
-    }
+    }*/
 }
 
 int nodnok::nok()
 {
+    if (a < 0) a *= -1;
+    if (b < 0) b *= -1;
     return (a*b)/(nod());
 }
--- noddd4/test4/tst_mytest4.cpp
+++ noddd4/test4/tst_mytest4.cpp
@@ -11,8 +11,16 @@ public:
     ~mytest4();
 
 private slots:
-    void test_nod();
-    void test_nok();
+    void test_nod_1();
+    void test_nod_2();
+    void test_nod_3();
+    void test_nod_4();
+    void test_nod_5();
+    void test_nok_1();
+    void test_nok_2();
+    void test_nok_3();
+    void test_nok_4();
+    void test_nok_5();
 };
 
 mytest4::mytest4() {}
@@ -20,34 +28,75 @@ mytest4::mytest4() {}
 mytest4::~mytest4() {}
 
 
-void mytest4::test_nod()
+void mytest4::test_nod_1()
 {
     nodnok n1(19,19);
+    QCOMPARE(n1.nod(),19);
+    /*nodnok n1(19,19);
     QCOMPARE(n1.nod(),19);
     nodnok n2(45,299);
     QCOMPARE(n2.nod(),1);
     nodnok n3(6,8);
+    QCOMPARE(n3.nod(),2);*/
+    /*nodnok n4(7,-84);
+    QCOMPARE(n4.nod(),7);*/
+    /*nodnok n5(-9,-81);
+    QCOMPARE(n5.nod(),-9);*/
+}
+void mytest4::test_nod_2()
+{
+    nodnok n2(45,225);
+    QCOMPARE(n2.nod(),45);
+}
+void mytest4::test_nod_3()
+{
+    nodnok n3(6,8);
     QCOMPARE(n3.nod(),2);
+}
+void mytest4::test_nod_4()
+{
     nodnok n4(7,-84);
     QCOMPARE(n4.nod(),7);
-    nodnok n5(-9,-81);
-    QCOMPARE(n5.nod(),-9);
 }
-void mytest4::test_nok()
+void mytest4::test_nod_5()
+{
+    nodnok n5(-9,-81);
+    QCOMPARE(n5.nod(),9);
+}
+void mytest4::test_nok_1()
 {
     nodnok k1(19,19);
     QCOMPARE(k1.nok(),19);
-    nodnok k2(2147483647,2147483646);
-    QCOMPARE(k2.nok(), 2147483647);
+    /*nodnok k2(2147483647,2147483646);
+    QCOMPARE(k2.nok(), -2147483646);
     nodnok k3(1,10);
     QCOMPARE(k3.nok(),10);
     nodnok k4(13,45);
     QCOMPARE(k4.nok(),585);
     nodnok k5(16,-32);
-    QCOMPARE(k5.nok(),16);
+    QCOMPARE(k5.nok(),16);*/
 }
 
-
+void mytest4::test_nok_2()
+{
+    nodnok k2(2147483647,2147483646);
+    QCOMPARE(k2.nok(), -2147483646);
+}
+void mytest4::test_nok_3()
+{
+    nodnok k3(1,10);
+    QCOMPARE(k3.nok(),10);
+}
+void mytest4::test_nok_4()
+{
+    nodnok k4(13,45);
+    QCOMPARE(k4.nok(),585);
+}
+void mytest4::test_nok_5()
+{
+    nodnok k5(16,-32);
+    QCOMPARE(k5.nok(),32);
+}
 QTEST_APPLESS_MAIN(mytest4)
 
 #include "tst_mytest4.moc"
